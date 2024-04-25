b83c1d199c14d589990fb1a1468a7ffd315409e7
satokogruson <satokogruson@gmail.com>
2024-04-25 11:53:29 +0200

    added javascript code to fetch time

---
+++ src/index.js
@@ -0,0 +1,28 @@
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement = newYorkElement.querySelector(".date");
+let newYorkTimeElement = newYorkElement.querySelector(".time");
+let newYorkTime = moment().tz("America/New_York");
+newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss  [<small>]A[</small>]");
+
+let melbourneElement = document.querySelector("#melbourne");
+let melbourneDateElement = melbourneElement.querySelector(".date");
+let melbourneTimeElement = melbourneElement.querySelector(".time");
+let melbourneTime = moment().tz("Australia/Melbourne");
+melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
+melbourneTimeElement.innerHTML = melbourneTime.format("h:mm:ss  [<small>]A[</small>]");
+
+
+let tokyoElement = document.querySelector("#tokyo");
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime = moment().tz("Asia/Tokyo");
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss  [<small>]A[</small>]");
+
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment().tz("Europe/Paris");
+parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+parisTimeElement.innerHTML = parisTime.format("h:mm:ss  [<small>]A[</small>]");
